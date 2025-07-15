b99f01c0762707c2e59636fc197ae417c94844b1
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-15 13:50:53 +0800

    20250715 v3

--- api/subscription.py
+++ api/subscription.py
@@ -5,3 +5,7 @@ subscription = APIRouter()
 @subscription.get("/",summary="subscription get summary",description="subscription get description")
 async def subscription_get():
   return {"subscription":"subscription"}
+
+@subscription.get("/{name}",summary="subscription get by name summary",description="subscription get by name description")
+async def subscription_get_by_name(name):
+  return {"subscription":name}
