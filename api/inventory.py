deb583066b9dc08255d81ad85d94c1df46573613
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-15 18:22:34 +0800

    20250715 v10

--- api/inventory.py
+++ api/inventory.py
@@ -14,6 +14,14 @@ async def inventory_get():
 @inventory.get("/{name}")
 async def get_inventory_by_name(name: str):
     try:
+        result = list_documents_by_name(name, collection_id="6876282f0003d93246fe")
+        return result
+    except Exception as e:
+        return {"error": str(e)}
+
+@inventory.get("/search/{name}")
+async def get_inventory_by_name(name: str):
+    try:
         result = list_documents_by_name_part(name, collection_id="6876282f0003d93246fe")
         return result
     except Exception as e:
