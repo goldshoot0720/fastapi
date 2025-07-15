4dce275754aafc58b25b317d65d52576eed92764
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-15 16:31:21 +0800

    20250715 v8

--- upto-git.bat
+++ upto-git.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\python\fastapi"
 git init
 git add .
-git commit -m "20250715 v7"
+git commit -m "20250715 v8"
 git remote add origin https://github.com/goldshoot0720/fastapi.git
 git checkout -b master
 git push -u origin master --force
