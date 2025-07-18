f3ad95244e67d3b9d7acb4bc9e192bf1ae6bcc7d
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-18 19:38:54 +0800

    20250718 v1

--- api/mail.py
+++ api/mail.py
@@ -7,13 +7,13 @@ mail = APIRouter()
 
 @mail.get("/", summary="List mail")
 async def mail_get():
-  result = list_documents(collection_id="6875d41e001e107081ad")
+  result = list_documents(collection_id="687a15df000014dff513")
   return result
 
 @mail.get("/{name}")
 async def get_mail_by_name(name: str):
     try:
-        result = list_documents_by_name(name, collection_id="6875d41e001e107081ad")
+        result = list_documents_by_name(name, collection_id="687a15df000014dff513")
         return result
     except Exception as e:
         return {"error": str(e)}
